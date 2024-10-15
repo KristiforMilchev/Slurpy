a7b14b6931079ff82d4843977240638d6166bbbd
Kristifor Milchev <km@airzen.io>
2024-10-15 18:10:36 +0300

    Refactoring and registering new commands.

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 
 	"Slurpy/commands"
+	"Slurpy/commands/network"
+	"Slurpy/commands/wallet"
 	"Slurpy/implementations"
 	"Slurpy/interfaces"
 )
@@ -20,19 +21,8 @@ func main() {
 	configuration = &implementations.Configuration{}
 	configuration.Load()
 
-	storage := setupDatabase(configuration)
-
-	locator = implementations.Locator{
-		WalletService: &implementations.WalletService{
-			Storage: storage,
-		},
-		EncoderService: &implementations.EncoderService{},
-		RpcService:     &implementations.RpcService{},
-		Storage:        storage,
-		DeploymentService: &implementations.DeploymentService{
-			Storage: storage,
-		},
-	}
+	storage := SetupDatabase(configuration)
+	locator = Locator(storage)
 
 	var rootCmd = &cobra.Command{Use: "slurpy"}
 
@@ -49,33 +39,37 @@ func main() {
 	deployCommand := commands.DeployCommand{
 		Locator: locator,
 	}
+	addWalletCommand := wallet.AddWallet{
+		Locator: locator,
+	}
+	addNetworkCommand := network.AddNetwork{
+		Locator: locator,
+	}
+
+	rpc := "HTTP://127.0.0.1:7545"
+	networkId := 5777
+	network := "local"
+
+	addNetworkCommand.Execute(&rpc, &networkId, &network)
+
+	privateKey := "bfc9f203f59ea2b5d664f0b6101df961dc834e6e8cbca6a1c364b9b57ae3f04f"
+
+	addWalletCommand.Execute(&privateKey, &network)
+
+	path := "deployment.json"
+	key := "test"
+
+	deployCommand.Execute(&path, &key, &network)
 
 	rootCmd.AddCommand(all.Executable())
 	rootCmd.AddCommand(configCommand.Executable())
 	rootCmd.AddCommand(getCommand.Executable())
 	rootCmd.AddCommand(deployCommand.Executable())
+	rootCmd.AddCommand(addWalletCommand.Executable())
+	rootCmd.AddCommand(addNetworkCommand.Executable())
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func setupDatabase(config interfaces.Configuration) interfaces.Storage {
-	storage := &implementations.Storage{}
-
-	dbPath := config.GetKey("DbPath").(string)
-	dbName := config.GetKey("DbName").(string)
-
-	tablesData, err := os.ReadFile("db.sql")
-
-	if err != nil {
-		log.Fatal("Couldn't find database file, aborting!")
-	}
-
-	tables := string(tablesData)
-	storage.New(&dbPath, &dbName, &tables)
-	storage.Open()
-	storage.Initialize()
-	return storage
 }
