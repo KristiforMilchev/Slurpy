cc235971f321e73396225a64b85f71a8f50664c4
Kristifor Milchev <km@airzen.io>
2024-10-15 18:08:12 +0300

    Moving the database setup and initialization outside of main.

---
+++ storage_config.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"Slurpy/implementations"
+	"Slurpy/interfaces"
+)
+
+func SetupDatabase(config interfaces.Configuration) interfaces.Storage {
+	storage := &implementations.Storage{}
+
+	dbPath := config.GetKey("DbPath").(string)
+	dbName := config.GetKey("DbName").(string)
+
+	tablesData, err := os.ReadFile("db.sql")
+
+	if err != nil {
+		log.Fatal("Couldn't find database file, aborting!")
+	}
+
+	tables := string(tablesData)
+	storage.New(&dbPath, &dbName, &tables)
+	storage.Open()
+	storage.Initialize()
+	return storage
+}
