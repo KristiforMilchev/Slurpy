4838a6d4755ca51446eaeac0e6d009f9ec3783b3
Kristifor Milchev <km@airzen.io>
2024-10-15 18:07:53 +0300

    Moving the depdency injector to a separate file.

---
+++ locator.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"Slurpy/implementations"
+	"Slurpy/interfaces"
+)
+
+func Locator(storage interfaces.Storage) implementations.Locator {
+	return implementations.Locator{
+		WalletService: &implementations.WalletService{
+			Storage: storage,
+		},
+		EncoderService: &implementations.EncoderService{},
+		RpcService:     &implementations.RpcService{},
+		Storage:        storage,
+		DeploymentService: &implementations.DeploymentService{
+			Storage: storage,
+		},
+		NetworkService: &implementations.NetworkService{
+			Storage: storage,
+		},
+	}
+}
