224dc6c1ab543a1c1cefd6ab4a422a6fb16bab7d
Kristifor Milchev <km@airzen.io>
2024-10-16 22:56:29 +0300

    adjusting all deployments to follow the new testable pattern.

--- commands/all.go
+++ commands/all.go
@@ -18,20 +18,24 @@ func (c *AllCommand) Executable() *cobra.Command {
 		Use:   "all",
 		Short: "List all deployments",
 		Run: func(cmd *cobra.Command, args []string) {
-			deployments, err := c.Locator.DeploymentService.GetDeployments()
-			if err != nil {
-				fmt.Println("Failed to get deployments, received an error!")
-				fmt.Println(err)
-			}
-
-			for _, deployment := range deployments {
-				fmt.Println(deployment.Contract)
-				fmt.Println(deployment.Date)
-			}
-
-			if len(deployments) == 0 {
-				fmt.Println("No deployments found")
-			}
+			c.Execute()
 		},
+	}
+}
+
+func (c *AllCommand) Execute() {
+	deployments, err := c.Locator.DeploymentService.GetDeployments()
+	if err != nil {
+		fmt.Println("Failed to get deployments, received an error!")
+		fmt.Println(err)
+	}
+
+	for _, deployment := range deployments {
+		fmt.Println(deployment.Contract)
+		fmt.Println(deployment.Date)
+	}
+
+	if len(deployments) == 0 {
+		fmt.Println("No deployments found")
 	}
 }
