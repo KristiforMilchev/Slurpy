965b00aa4368591d4e96e7c478b17bc91fff2eb5
Kristifor Milchev <km@airzen.io>
2024-10-19 16:22:28 +0300

    Updating the deployment file structure.

--- models/contract_config.go
+++ models/contract_config.go
@@ -3,5 +3,5 @@ package models
 type ContractConfig struct {
 	Bytecode     string        `json:"bytecode"`
 	Abi          []interface{} `json:"abi"`
-	Dependencies []interface{} `json:"dependencies"`
+	Dependencies []Dependency  `json:"dependencies"`
 }
---
+++ models/depdendency.go
@@ -0,0 +1,6 @@
+package models
+
+type Dependency struct {
+	Value string `json:"value"`
+	Type  string `json:"type"`
+}
