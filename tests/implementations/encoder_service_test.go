7a968c662dd223a51b869d9743bef76c3dee3b0b
Kristifor Milchev <km@airzen.io>
2024-10-29 23:32:47 +0200

    refactoring the encoder service to work with mocks.

--- tests/implementations/encoder_service_test.go
+++ tests/implementations/encoder_service_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/ovechkin-dm/mockio/mock"
+
 	"slurpy/implementations"
 	"slurpy/interfaces"
 )
@@ -11,6 +13,7 @@ import (
 var encoder interfaces.EncoderService
 
 func Test_Should_Parse_Valid_ABI(t *testing.T) {
+	mock.SetUp(t)
 	encoder = &implementations.EncoderService{}
 
 	file, err := os.ReadFile("../test_data/example_abi.json")
@@ -30,6 +33,7 @@ func Test_Should_Parse_Valid_ABI(t *testing.T) {
 }
 
 func Test_Should_Fail_To_Parse_Empty_ABI(t *testing.T) {
+	mock.SetUp(t)
 	encoder = &implementations.EncoderService{}
 	var empty []byte
 	_, err := encoder.ParseAbi(&empty)
@@ -40,6 +44,7 @@ func Test_Should_Fail_To_Parse_Empty_ABI(t *testing.T) {
 }
 
 func Test_Parse_Bytecode_From_Valid_Hex(t *testing.T) {
+	mock.SetUp(t)
 	encoder = &implementations.EncoderService{}
 	bytecode := "0x6080604052348015600f57600080fd5b5060405160c838038060c8833981016040819052602a91604e565b600080546001600160a01b0319166001600160a01b0392909216919091179055607c565b600060208284031215605f57600080fd5b81516001600160a01b0381168114607557600080fd5b9392505050565b603f8060896000396000f3fe6080604052600080fdfea2646970667358221220164ced733355373d04d2603f54a7b92e4ba5abdfc29a3db4485038f2f79773a064736f6c63430008130033"
 
@@ -53,6 +58,7 @@ func Test_Parse_Bytecode_From_Valid_Hex(t *testing.T) {
 }
 
 func Test_Parse_Bytecode_From_Valid_Hex_Without_0x(t *testing.T) {
+	mock.SetUp(t)
 	encoder = &implementations.EncoderService{}
 	bytecode := "6080604052348015600f57600080fd5b5060405160c838038060c8833981016040819052602a91604e565b600080546001600160a01b0319166001600160a01b0392909216919091179055607c565b600060208284031215605f57600080fd5b81516001600160a01b0381168114607557600080fd5b9392505050565b603f8060896000396000f3fe6080604052600080fdfea2646970667358221220164ced733355373d04d2603f54a7b92e4ba5abdfc29a3db4485038f2f79773a064736f6c63430008130033"
 
@@ -66,6 +72,7 @@ func Test_Parse_Bytecode_From_Valid_Hex_Without_0x(t *testing.T) {
 }
 
 func Test_Error_If_Invalid_Hex_Is_Provided(t *testing.T) {
+	mock.SetUp(t)
 	encoder = &implementations.EncoderService{}
 	invalidHex := []string{
 		"0xGHIJKL",   // Invalid characters
